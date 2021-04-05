3ccd185cb6d700d05ad4786461558fbea24162a9
TomasBerdis <tomasberdis@gmail.com>
2021-04-05 20:31:24 +0200

    Updated wind function

--- shaders/grassVS.glsl
+++ shaders/grassVS.glsl
@@ -23,6 +23,16 @@ layout(std430, binding=0) buffer patchTranslationBuffer
     mat4 patchTranslations[];
 };
 
+/* Wind function */
+float w(vec3 p)
+{
+   int c1 = 1;
+   int c2 = 1;
+   int c3 = 1;
+   float a = M_PI * p.x + uTime/150 + (M_PI / 4) / (abs(cos(c2 * M_PI * p.z)) + 0.00001);
+   return sin(c1 * a) * cos(c3 * a);
+}
+
 void main()
 {
    /* Calculate world space position */
@@ -39,10 +49,14 @@ void main()
    vec4 heightSample = texture(uHeightMap, mapCoords);
 
    /* Apply random variation influenced by world space position */
-   float c = 0.5;
+   float c = 42;
    float r0 = position.w       * sin(c * worldPos.x);
    float r1 = centerPosition.w * sin(c * worldPos.z);
    float r2 = texCoord.z       * sin(c * worldPos.x);
+   vPosition.w       = position.w       * sin(c * worldPos.x);
+   vCenterPosition.w = centerPosition.w * sin(c * worldPos.x);
+   vTexCoord.zw      = texCoord.zw      * sin(c * worldPos.x);
+   vRandoms          = randoms          * sin(c * worldPos.x);
 
    float angle = 2 * M_PI * r0;
    float xDelta = position.x - centerPosition.x;
@@ -56,12 +70,14 @@ void main()
       newZ = newZ + (uMaxBendingFactor * (2 * r2 - 1));
    }
 
-   /* Wind function */
+   /* Wind calculation */
    if ((centerPosition.y > 0.99f) && (uWindEnabled == 1)) // upper vertices
    {
       /* Inspired by Horizon Zero Dawn GDC presentation */
-      newX = newX + (2 * sin (1 * (worldPos.x + worldPos.y + worldPos.z + uTime/150))) + 1;
-      newZ = newZ + (1 * sin (2 * (worldPos.x + worldPos.y + worldPos.z + uTime/150))) + 0.5;
+      // newX = newX + (2 * sin (1 * (worldPos.x + worldPos.y + worldPos.z + uTime/150))) + 1;
+      // newZ = newZ + (1 * sin (2 * (worldPos.x + worldPos.y + worldPos.z + uTime/150))) + 0.5;
+      newX = newX + w(worldPos);
+      newZ = newZ + w(worldPos);
    }
 
    /* New height sampled from height map */
